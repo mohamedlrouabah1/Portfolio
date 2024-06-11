7bc4d38f5ebfc1a34c22b5ee137fde111fa81720
mohamedlrouabah1 <mohamedlrouabah@icloud.com>
2024-06-11 12:07:12 +0200

    new file:   src/Assets/Projects/alpestransport.png 	modified:   src/components/Projects/Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import onconnect from "../../Assets/Projects/onconnect.png";
-
+import alpesTransport from "../../Assets/Projects/alpestransport.png";
+import StellaStone from "../../Assets/Projects/StellaStone.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -28,25 +29,25 @@ function Projects() {
           </Col>
 
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={bitsOfCode}
+            <ProjectCard
+              imgPath={alpesTransport}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            /> */}
+              title="alpesTransport"
+              description="Wikibase is an open-source platform developed by the Wikimedia Foundation that stores, manages and publishes structured and linked data using Semantic Web technologies such as RDF (Resource Description Framework) and SPARQL (SPARQL Protocol and RDF Query Language)."
+              ghLink="https://github.com/mohamedlrouabah1/alpestransport"
+              demoLink="https://mohamedlrouabah1.github.io/alpestransport/"
+            />
           </Col>
 
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={editor}
+            <ProjectCard
+              imgPath={StellaStone}
               isBlog={false}
-              title="Editor.io"
+              title="StellaStone"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            /> */}
+              ghLink="https://github.com/mohamedlrouabah1/StellaStone"
+              demoLink="https://mohamedlrouabah1.github.io/StellaStone"              
+            />
           </Col>
 
           <Col md={4} className="project-card">
