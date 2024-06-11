52075dcfaf96f853caf14f6de0191d31051e2d91
mohamedlrouabah1 <mohamedlrouabah@icloud.com>
2024-06-11 14:21:11 +0200

    new file:   src/Assets/Projects/foodies.png 	modified:   src/components/Projects/Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import onconnect from "../../Assets/Projects/onconnect.png";
 import alpesTransport from "../../Assets/Projects/alpestransport.png";
 import StellaStone from "../../Assets/Projects/StellaStone.png";
+import Foodies from "../../Assets/Projects/foodies.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -35,7 +36,7 @@ function Projects() {
               title="alpesTransport"
               description="Wikibase is an open-source platform developed by the Wikimedia Foundation that stores, manages and publishes structured and linked data using Semantic Web technologies such as RDF (Resource Description Framework) and SPARQL (SPARQL Protocol and RDF Query Language)."
               ghLink="https://github.com/mohamedlrouabah1/alpestransport"
-              demoLink="https://mohamedlrouabah1.github.io/alpestransport/"
+              // demoLink="https://mohamedlrouabah1.github.io/alpestransport/"
             />
           </Col>
 
@@ -46,19 +47,30 @@ function Projects() {
               title="StellaStone"
               description="The StellaStone app is a program designed to perform space travel simulations. It offers you the opportunity to experience or relive historical moments, to explore and deepen your knowledge of space while having fun. Learning how to use this software is not complex, but requires a good understanding of the terms, concepts, and methods used in the application. It is recommended that you consult this manual before using the software. This user guide provides an overview of the app's features and features, as well as instructions on how to master these various features."
               ghLink="https://github.com/mohamedlrouabah1/StellaStone"
-              demoLink="https://mohamedlrouabah1.github.io/StellaStone"              
+              // demoLink="https://mohamedlrouabah1.github.io/StellaStone"              
             />
           </Col>
-
           <Col md={4} className="project-card">
-            {/* <ProjectCard
-              imgPath={leaf}
+            <ProjectCard
+              imgPath={Foodies}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            /> */}
+              title="Foodies"
+              description="The Foodies application main use is to use RDFs graph scrapped from website page and display them in format of annuary of restaurants and their menus."
+              ghLink="https://github.com/mohamedlrouabah1/WEBSEM"
+              demoLink="https://foodies-ldp-e2f3380fb9a9.herokuapp.com/"  
+              demoLink2="https://dsc2-sw-food-delivery-b3a7e3e908fb.herokuapp.com/#/"            
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Foodies}
+              isBlog={false}
+              title="Foodies"
+              description="The Foodies application main use is to use RDFs graph scrapped from website page and display them in format of annuary of restaurants and their menus."
+              ghLink="https://github.com/mohamedlrouabah1/WEBSEM"
+              demoLink="https://foodies-ldp-e2f3380fb9a9.herokuapp.com/"  
+              demoLink2="https://dsc2-sw-food-delivery-b3a7e3e908fb.herokuapp.com/#/"            
+            />
           </Col>
         </Row>
       </Container>
